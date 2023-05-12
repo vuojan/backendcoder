0bde0558916c68a547942b9b456f448c3f53fca6
geronimo <gero.amuchastegui@gmail.com>
2023-05-11 21:02:27 -0300

    entrega 3

---
+++ src/ProductManager.js
@@ -0,0 +1,75 @@
+import fs from "fs"
+
+export default class ProductManager {
+
+    constructor() {
+        this.path = "\Products.json";
+    }
+
+    addProduct = async(product) => {
+        const products = await this.getProducts();
+
+        if (products.length === 0){
+            product.id = 1;
+            } else{
+            product.id = products[products.length - 1].id + 1
+            } 
+
+        products.push(product)
+        await fs.promises.writeFile(this.path, JSON.stringify(products, null, '\t'))
+        return products
+
+        }
+        
+
+    getProducts = async() => {
+
+        if (fs.existsSync(this.path)){
+            
+            const dataProduct = await fs.promises.readFile(this.path, 'utf-8')
+            const products = JSON.parse(dataProduct)
+            return products
+
+        } else {
+            return []
+        }
+
+    }
+
+    getProductById = async(id) => {
+
+         const products = await this.getProducts()
+
+         const product = products.find(product => product.id === id)
+        
+         return product
+
+
+     }
+
+    updateProduct = async(id) => {
+
+        const products = await this.getProducts()
+
+        const updatedProduct = products.find(product => product.id === id)
+        updatedProduct.stock = 10
+
+        await fs.promises.writeFile(this.path, JSON.stringify(products, null, '\t'))
+       
+        return updatedProduct
+
+    }
+
+    deleteProduct = async(id) => {
+
+         const products = await this.getProducts()
+
+         const newProducts = products.filter(product => product.id !== id)
+         
+         await fs.promises.writeFile(this.path, JSON.stringify(newProducts, null, '\t'))
+
+        return newProducts
+
+        }
+
+}
